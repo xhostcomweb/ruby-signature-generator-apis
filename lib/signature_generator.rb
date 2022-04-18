9183502e452d3a1bb0e6b016fe66a9cdb215bfe0
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-18 10:15:49 +0100

    Setup Commit

---
+++ lib/signature_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "signature_generator/version"
+
+module SignatureGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/signature_generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SignatureGenerator do
+  it "has a version number" do
+    expect(SignatureGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
