a25816f91a2cb7b2c32e7ccb6839831466aa7834
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-22 07:40:29 +0100

    Update

--- lib/signature_generator/escher_loader.rb
+++ lib/signature_generator/escher_loader.rb
@@ -4,12 +4,11 @@ require_relative "../middleware/rack_middleware"
 
 module SignatureGenerator
   class EscherLoader
-    def initialize(escher_file)
-      @escher_file = escher_file
+    def initialize(path)
+      @path = path
     end
-
-    def load
-      Escher.load(@escher_file)
-    end
+    # { File.expand_path("../../lib/middleware/rack_middleware.rb", __dir__) }
+    #def load; end
+    # end
   end
 end
--- spec/escher_loader_spec.rb
+++ spec/escher_loader_spec.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe SignatureGenerator::EscherLoader do
+  let(:loader) { described_class.new(path) }
+  let(:path) { "spec/fixtures/escher_loader_spec.rb" }
+  # let(:path) { File.expand_path("fixtures/lines.log", __dir__) }
   describe "#load" do
-    let(:escher_loader) { EscherLoader.new }
-    let(:escher_file) { File.join(File.dirname(__FILE__), "../../lib/middleware/rack_middleware.rb") }
-    it "loads the escher middleware config file" do
-      expect(escher_loader.load(escher_file)).to have_class(Escher::RackMiddleware)
+    subject(:path) { loader.load(filename) }
+    it "returns the path to escher-rack-middleware file" do
+      expect(path).to eq(path)
     end
-  end
+    it "has a public method add_escher_authenticator" do
+      expect(SignatureGenerator.respond_to?(:add_escher_authenticator)).to be true
+    end
 
-  it "has a public method add_escher_authenticator" do
-    expect(SignatureGenerator.respond_to?(:add_escher_authenticator)).to be true
-  end
-
-  it "has a public method add_credential_updater" do
-    expect(SignatureGenerator.respond_to?(:add_credential_updater)).to be true
+    it "has a public method add_credential_updater" do
+      expect(SignatureGenerator.respond_to?(:add_credential_updater)).to be true
+    end
   end
 end
